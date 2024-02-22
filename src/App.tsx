c0c2fb36653d5b87cd9a28044cd98f8ec464dd52
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-02-22 11:54:44 +0530

    feat: added react, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState("")
+
+  return (
+    <>
+      <div className="bg-red-200"> a typescript project</div>
+    </>
+  )
+}
+
+export default App
