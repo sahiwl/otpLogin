93adce3b338f8ac820cc3e6cb404cd54d89db231
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-02-23 02:41:01 +0530

    feat: used routing in components

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import {LoginPage} from "./components/LoginPage.tsx";
 import OTP from './components/OTP.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
-  const [count, setCount] = useState("")
 
   return (
-    <>
-    {/* <LoginPage />     */}
-    <OTP/>
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/otp" element={<OTP/>}/>
+      </Routes>
+    </BrowserRouter>
+// NO-LAZYLOADING 
   )
 }
 
--- src/components/OTP.tsx
+++ src/components/OTP.tsx
@@ -1,20 +1,20 @@
 // import useNavigate from 'react-router-dom'
 
 export default function OTP(){
+    console.log("rendering otp page")
     return <>
-    <div className="bg-gray-700 h-screen w-screen flex justify-center items-center ">
-        <div className="h-1/2 w-1/2 bg-red-200 flex justify-center  outline grid grid-rows-4">Login via OTP
-            <div className="bg-blue-200 w-80 flex justify-center items-center grid grid-cols-4">
-                <div className="bg-pink-300 h-1/2 w-1/2 outline">abab</div>
-                <div className="bg-pink-300 h-1/2 w-1/2 outline">abab</div>
-                <div className="bg-pink-300 h-1/2 w-1/2 outline">abab</div>
-                <div className="bg-pink-300 h-1/2 w-1/2 outline">abab</div>
+    <div className="bg-gray-700 h-screen w-screen flex justify-center items-center">
+        <div className="h-1/2 w-1/2 flex justify-center  outline grid grid-rows-3 text-center pt-2 rounded-xl"> 
+        <div className=" h-20 flex justify-center items-center font-semibold text-4xl"> Login via OTP</div>
+            <div className=" w-80 flex justify-center items-center grid grid-cols-4 ">
+                <div className="h-1/3 w-1/2 outline rounded-sm"></div>
+                <div className=" h-1/3 w-1/2 outline rounded-sm"></div>
+                <div className=" h-1/3 w-1/2 outline rounded-sm"></div>
+                <div className=" h-1/3 w-1/2 outline rounded-sm"></div>
             </div>
-            <div className="bg-green-200 flex justify-center items-center">
-                <button className="text-gray-300 bg-gray-900 border border-gray-300 rounded-xl font-medium text-sm px-5 py-2.5 me-2 mb-2">Login</button>
+            <div className="flex justify-center items-center">
+                <button className="text-gray-300 bg-gray-900 border border-gray-400 rounded-xl font-medium text-sm px-5 py-2.5 me-2 mb-2">Login</button>
             </div>
-            <div className="bg-yellow-200"></div>
-
         </div>
     </div>
 
