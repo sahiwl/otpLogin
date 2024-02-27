d58192c71d9037968a79cdeeb77e421bf5889f9f
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-02-27 23:29:06 +0530

    input boxes in otp.tsx

--- src/components/OTP.tsx
+++ src/components/OTP.tsx
@@ -8,11 +8,10 @@ export default function OTP(){
         <div className="h-1/2 w-1/2 flex justify-center  outline grid row-span-4 grid-rows-3 text-center pt-2 rounded-xl" style={{width: "30rem"}}> 
             <div className=" h-20 flex justify-center items-center font-semibold text-4xl"> Login via OTP</div>
             <div className=" w-80 flex justify-center items-center grid grid-cols-4 ">
-                    <div className="h-1/3 w-1/2 outline rounded-sm"></div>
-                    {/* <input type="text" className="h-1/3 w-1/2 outline rounded-sm bg-gray-700" /> */}
-                    <div className=" h-1/3 w-1/2 outline rounded-sm"></div>
-                    <div className=" h-1/3 w-1/2 outline rounded-sm"></div>
-                    <div className=" h-1/3 w-1/2 outline rounded-sm"></div>
+                    <input type="tel" pattern="[0-9]{1}" maxLength="1" className="h-1/3 w-1/2 outline rounded-sm bg-gray-700 font-medium text-center" />
+                    <input type="tel" pattern="[0-9]{1}" maxLength="1" className="h-1/3 w-1/2 outline rounded-sm bg-gray-700 font-medium text-center" />
+                    <input type="tel" pattern="[0-9]{1}" maxLength="1" className="h-1/3 w-1/2 outline rounded-sm bg-gray-700 font-medium text-center" />
+                    <input type="tel" pattern="[0-9]{1}" maxLength="1" className="h-1/3 w-1/2 outline rounded-sm bg-gray-700 font-medium text-center" />
             </div>
             <div className="flex justify-center items-center">
                 <button className="text-gray-300 bg-gray-900 border border-gray-400 rounded-xl font-medium text-sm px-5 py-2.5 me-2 mb-2">Login</button>
